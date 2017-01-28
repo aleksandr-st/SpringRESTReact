d459dcf7218ba61ae8f7039140599fb20a7e58a3
aleksandr-st <stupakov.aleksandr.2014@gmail.com>
2017-01-28 18:29:48 +0200

    Add javascript files and correct config files

---
+++ src/main/js/api/uriListConverter.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Sasha on 28.01.2017.
+ */
+define (function () {
+    'use strict';
+
+    return {
+        read: function(str /*, opts */) {
+            return str.split('\n');
+        },
+        write: function(obj /*, opts */) {
+            // If this is an Array, extract the self URI and then join using a newline
+            if (obj instanceof Array) {
+                return obj.map(function(resource) {
+                    return resource._links.self.href;
+                }).join('\n');
+            } else { // otherwise, just return the self URI
+                return obj._links.self.href;
+            }
+        }
+    };
+
+});
